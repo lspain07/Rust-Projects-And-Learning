2341663a0174df71da7c34e033f9330c4ba43afb
lspain07 <lukasrspain@gmail.com>
2025-08-22 14:23:28 -0500

    huh

---
+++ data-types/target/debug/data-types.d
@@ -0,0 +1,1 @@
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\data-types\target\debug\data-types.exe: C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\data-types\src\main.rs
---
+++ data-types/target/debug/deps/data_types.d
@@ -0,0 +1,5 @@
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\data-types\target\debug\deps\data_types.d: src\main.rs
+
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\data-types\target\debug\deps\data_types.exe: src\main.rs
+
+src\main.rs:
