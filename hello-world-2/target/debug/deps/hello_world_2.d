218cde9e0c69847db436ea9785bdedc6501a2038
lspain07 <lukasrspain@gmail.com>
2025-09-17 22:59:55 -0500

    RUST BOOK START

---
+++ hello-world-2/target/debug/deps/hello_world_2.d
@@ -0,0 +1,5 @@
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\hello-world-2\target\debug\deps\hello_world_2.d: src\main.rs
+
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\hello-world-2\target\debug\deps\hello_world_2.exe: src\main.rs
+
+src\main.rs:
---
+++ hello-world-2/target/debug/hello-world-2.d
@@ -0,0 +1,1 @@
+C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\hello-world-2\target\debug\hello-world-2.exe: C:\Users\lukas\OneDrive\Documents\GitHub\Rust-Projects-And-Learning\hello-world-2\src\main.rs
